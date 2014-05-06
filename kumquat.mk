be0e587c7d19ba220158cd5051252edf2052bd8d
Nathanaël Mautard <admin@info4geek.com>
2014-05-06 11:15:58 +0200

    Welcome back OTA Updater

--- kumquat.mk
+++ kumquat.mk
@@ -93,4 +93,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.sf.lcd_density=240 \
   ro.config.low_ram=true
 
-
+# OTA Updater
+PRODUCT_PROPERTY_OVERRIDES += otaupdater.otaid=buildid
+PRODUCT_PROPERTY_OVERRIDES += otaupdater.otaver=buildver
+PRODUCT_PROPERTY_OVERRIDES += otaupdater.otatime=buildtime
+PRODUCT_PACKAGES += OTAUpdater
