5993667422d6f46dfca53a78aa50e28ef9c79f0e
Agontuk <rifat662@gmail.com>
2014-09-11 02:45:24 +0600

    Fix Illumination service on startup
    Conflicts:
    	config/init.st-ericsson.device.rc

--- kumquat.mk
+++ kumquat.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/logo-480x854.rle:root/logo.rle
 $(call inherit-product, $(LOCAL_PATH)/prebuilt/resources-480x854.mk)
 
+# Illumination Fix
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/config/led_fix.sh:system/bin/led_fix.sh
+
 # TWRP
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/twrp.fstab:recovery/root/etc/twrp.fstab
